8ca3d43bf8d53305713de3c4a6b7d287189d778e
Jake Krayger <jkrayg@protonmail.com>
2020-04-27 16:27:15 -0400

    Adding functions -_-

--- bamazon.js
+++ bamazon.js
@@ -13,53 +13,54 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connection Successful!");
     establishedConnection()
-    //userPrompt()
-    //connection.end();
 })
 
 function establishedConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         console.log(res);
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "itemID",
-                message: "What is the item ID of the product you would like to purchase?"
-            },
-            {
-                type: "input",
-                name: "units",
-                message: "What is the quantity of this product you would like to purchase?"
-            }
-        ]).then(function(item) {
-            var theItem = item.itemID;
-            connection.query(
-                "SELECT stock_quantity FROM products WHERE ?",
-                {
-                    id: theItem
-                }
-                ,function(err, res) {
-                    if (err) throw err;
-                    if (item.units <= /*available stock [ RowDataPacket { stock_quantity: 22 } ]*/ ) {
-                        connection.query(
-                            "UPDATE products SET ? WHERE ?", 
-                            [
-                                {
-                                    stock_quantity: //stock_quantity - item.units
-                                },
-                                {
-                                    id: itemID
-                                }
-                            ]
-                        )
-                    }
-                    console.log(res);
-                    connection.end();
-                }
-            )
-        })
-         
+        customerPrompt();
     });
     
+}
+
+function customerPrompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemID",
+            message: "What is the item ID of the product you would like to purchase?"
+        },
+        {
+            type: "input",
+            name: "units",
+            message: "What is the quantity of this product you would like to purchase?"
+        }
+    ]).then(function(item) {
+        var theItem = item.itemID;
+        connection.query(
+            "SELECT stock_quantity FROM products WHERE ?",
+            {
+                id: theItem
+            }
+            ,function(err, res) {
+                if (err) throw err;
+                // if (item.units <= /*available stock || [ RowDataPacket { stock_quantity: 22 } ]*/ ) {
+                //     connection.query(
+                //         "UPDATE products SET ? WHERE ?", 
+                //         [
+                //             {
+                //                 stock_quantity: /*stock_quantity - item.units*/
+                //             },
+                //             {
+                //                 id: itemID
+                //             }
+                //         ]
+                //     )
+                // }
+                console.log(res);
+                connection.end();
+            }
+        )
+    })
 }
